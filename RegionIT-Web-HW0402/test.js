3ba681129acaa6272089ac5ed2fee754c93c9222
Iryna <41705694+IrynaShumilova@users.noreply.github.com>
2018-08-14 09:09:15 +0300

    Add files via upload

---
+++ RegionIT-Web-HW0402/test.js
@@ -0,0 +1,37 @@
+﻿describe("problem12", function () {
+
+    it("test1",
+        function () {
+            var res = problem12(1);
+            assert.equal(2, res);
+        });
+});
+
+
+describe("problem13", function () {
+
+    it("test1",
+        function () {
+            var res = problem13(3.0198773448773446);
+            assert.equal(4, res);
+        });
+});
+
+describe("problem14", function () {
+
+    it("test1",
+        function () {            
+            assert.equal(problem14(2), 4);
+            assert.equal(problem14(3), 8);
+            assert.equal(problem14(10), 1024);
+        });
+});
+
+describe("problem15", function () {
+
+    it("test1",
+        function () {
+            var res = problem15(2);
+            assert.equal(2.5, res);
+        });
+});
